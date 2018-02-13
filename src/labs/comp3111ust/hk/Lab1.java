5c3371eb5fa0b5e2c78bf73e7367be6395531f5f
GG <logg.com@gmail.com>
2018-02-13 11:52:03 +0800

    Added a message

--- src/labs/comp3111ust/hk/Lab1.java
+++ src/labs/comp3111ust/hk/Lab1.java
@@ -4,6 +4,7 @@ public class Lab1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Lab 1: Introduction to Git and Github");
+		System.out.println("Congradulation! You learned the Git Basic");
 	}
 
 }
